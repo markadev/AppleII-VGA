73028c0fe02eec155678e7f80b68ed54293166a6
Mark Aikens <17986295+markadev@users.noreply.github.com>
2021-12-21 08:12:33 -0500

    Fix abus to work on a real Apple II bus.
    Set up text memory shadowing and fixed a few abus issues:
     * Keep input synchronizer enabled on the PHI0 clock because it
       messes up the PIO in weird ways.
     * Sample DEVSEL at an appropriate time within phase 0
    
    Text memory shadowing works now, as well as device read cycle handling

--- pico/abus.c
+++ pico/abus.c
@@ -1,18 +1,20 @@
 #include <string.h>
 #include <hardware/pio.h>
-#include <hardware/pwm.h>
-
 #include "config.h"
 #include "abus.h"
 #include "abus.pio.h"
+#include "buffers.h"
+
+
+#if CONFIG_PIN_APPLEBUS_PHI0 != PHI0_GPIO
+#error CONFIG_PIN_APPLEBUS_PHI0 and PHI0_GPIO must be set to the same pin
+#endif
 
 
 enum {
     ABUS_MAIN_SM = 0,
     ABUS_DEVICE_READ_SM = 1,
 };
-
-// TODO pre-processor check to assert that CONFIG_PIN_APPLEBUS_PHI0 == PHI0_GPIO
 
 
 static void abus_device_read_setup(PIO pio, uint sm) {
@@ -36,8 +38,6 @@ static void abus_device_read_setup(PIO pio, uint sm) {
 }
 
 static void abus_main_setup(PIO pio, uint sm) {
-    const enum gpio_function gpfn = (pio == pio0) ? GPIO_FUNC_PIO0 : GPIO_FUNC_PIO1;
-
     uint program_offset = pio_add_program(pio, &abus_program);
     pio_sm_claim(pio, sm);
 
@@ -66,18 +66,21 @@ static void abus_main_setup(PIO pio, uint sm) {
         (0xf << CONFIG_PIN_APPLEBUS_CONTROL_BASE),
         (1 << CONFIG_PIN_APPLEBUS_PHI0) | (0xf << CONFIG_PIN_APPLEBUS_CONTROL_BASE) | (0x3ff << CONFIG_PIN_APPLEBUS_DATA_BASE));
 
-    // Disable input synchronization on input pins to shave off two clock cycles of input latency
-    pio->input_sync_bypass |= (1 << CONFIG_PIN_APPLEBUS_PHI0) | (0x3ff << CONFIG_PIN_APPLEBUS_DATA_BASE);
+    // Disable input synchronization on input pins that are sampled at known stable times
+    // to shave off two clock cycles of input latency
+    pio->input_sync_bypass |= (0x3ff << CONFIG_PIN_APPLEBUS_DATA_BASE);
 
-    gpio_set_function(CONFIG_PIN_APPLEBUS_PHI0, gpfn);
+    pio_gpio_init(pio, CONFIG_PIN_APPLEBUS_PHI0);
+    gpio_set_pulls(CONFIG_PIN_APPLEBUS_PHI0, false, false);
     for(int pin=CONFIG_PIN_APPLEBUS_CONTROL_BASE; pin < CONFIG_PIN_APPLEBUS_CONTROL_BASE+4; pin++) {
-        gpio_set_function(pin, gpfn);
-        gpio_set_drive_strength(pin, GPIO_DRIVE_STRENGTH_2MA);
+        pio_gpio_init(pio, pin);
     }
     for(int pin=CONFIG_PIN_APPLEBUS_DATA_BASE; pin < CONFIG_PIN_APPLEBUS_DATA_BASE+10; pin++) {
-        gpio_set_function(pin, gpfn);
+        pio_gpio_init(pio, pin);
+        gpio_set_pulls(pin, false, false);
     }
 }
+
 
 void abus_init() {
     abus_device_read_setup(CONFIG_ABUS_PIO, ABUS_DEVICE_READ_SM);
@@ -86,58 +89,30 @@ void abus_init() {
     pio_enable_sm_mask_in_sync(CONFIG_ABUS_PIO, (1 << ABUS_MAIN_SM) | (1 << ABUS_DEVICE_READ_SM));
 }
 
+
 void abus_loop() {
-    if(0) {
-        // Start a square wave generator as a fake Phase 0 testing clock. This must be
-        // connected externally to PHI0_GPIO.
-        const uint test_gpio = 28;
-        const uint pwmSliceNum = pwm_gpio_to_slice_num(test_gpio);
-        const uint pwmChannel = pwm_gpio_to_channel(test_gpio);
-        gpio_set_function(test_gpio, GPIO_FUNC_PWM);
-        pwm_set_clkdiv(pwmSliceNum, 31.5);
-        pwm_set_wrap(pwmSliceNum, 3);           // Period of 4 cycles
-        pwm_set_chan_level(pwmSliceNum, pwmChannel, 2);  // 50% duty cycle
-        pwm_set_enabled(pwmSliceNum, true);
-    }
-
-    gpio_init(PICO_DEFAULT_LED_PIN);
-    gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
-
-    uint8_t mem[256];
-    memset(mem, 0xff, sizeof(mem));
-
-    uint8_t io[16];
-    memset(io, 0xff, sizeof(io));
-
-    const uint mem_mask = 0xff00;
-    const uint io_mask = 0xfff0;
-
-    uint mymemprefix = 0;
-    uint myioprefix = 0;
     while(1) {
         uint32_t value = pio_sm_get_blocking(CONFIG_ABUS_PIO, ABUS_MAIN_SM);
         uint address = (value >> 10) & 0xffff;
 
-#if 0
-        // FIXME: this is all untested, just to see if the CPU has enough time to respond to reads in the worst case
-        if((myioprefix == 0) && ((value & (1u << 8)) == 0)) {
-            // device is selected for the first time
-            myioprefix = address & io_mask;
-            mymemprefix = (((address & 0x00f0) - 0x0080) << 8) | 0xc000;
+        if((value & (1u << CONFIG_PIN_APPLEBUS_DEVSEL-CONFIG_PIN_APPLEBUS_DATA_BASE)) == 0) {
+            gpio_xor_mask(1u << PICO_DEFAULT_LED_PIN);
+
+            if((value & (1u << CONFIG_PIN_APPLEBUS_RW-CONFIG_PIN_APPLEBUS_DATA_BASE)) != 0) {
+                // device read access
+                pio_sm_put_blocking(CONFIG_ABUS_PIO, ABUS_DEVICE_READ_SM, address & 0xf);
+            }
         }
 
-        if((value & (3u << 8)) == (2u << 8)) {
-            // read cycle for this device
-            uint data;
-            if((address & io_mask) == myioprefix) {
-                data = io[address & 0xf];
-            } else if((address & mem_mask) == mymemprefix) {
-                data = mem[address & 0xff];
-            } else {
-                data = 0xff;
+        // $0400 - $07FF (1024 - 2047): Text Video Page and Peripheral Screenholes
+        // $0800 - $0BFF (2048 - 3071): Text Video Page 2 or Applesoft Program and Variables
+        if((value & (1u << CONFIG_PIN_APPLEBUS_RW-CONFIG_PIN_APPLEBUS_DATA_BASE)) == 0) {
+            // write cycle
+            if((address & 0xfc00) == 0x400) {
+                text_page[address & 0x3ff] = value & 0xff;
+            } else if((address & 0xfc00) == 0x800) {
+                text_page_2[address & 0x3ff] = value & 0xff;
             }
-            pio_sm_put(CONFIG_ABUS_PIO, ABUS_DEVICE_READ_SM, data);
         }
-#endif
     }
 }
