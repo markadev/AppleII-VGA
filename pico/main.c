583aa4e66c7a6b7786cf2f24a7ae5e3a0d7a10db
fgutier2 <fgutier2@jaguarlandrover.com>
2023-06-20 14:14:52 +0100

    made all formating changes

--- pico/main.c
+++ pico/main.c
@@ -5,22 +5,23 @@
 #include "render.h"
 #include "vga.h"
 
-static void core1_main()
-{
-  vga_init();
-  render_init();
-  render_loop();
+
+static void core1_main() {
+    vga_init();
+    render_init();
+    render_loop();
 }
 
-int main()
-{
-  // Adjust system clock for better dividing into other clocks
-  set_sys_clock_khz(CONFIG_SYSCLOCK * 1000, true); 
-  // Setup the on-board LED for debugging
-  gpio_init(PICO_DEFAULT_LED_PIN);
-  gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
-  multicore_launch_core1(core1_main);
+int main() {
+    // Adjust system clock for better dividing into other clocks
+    set_sys_clock_khz(CONFIG_SYSCLOCK*1000, true);
 
-  abus_init();
-  abus_loop();
+    // Setup the on-board LED for debugging
+    gpio_init(PICO_DEFAULT_LED_PIN);
+    gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
+
+    multicore_launch_core1(core1_main);
+
+    abus_init();
+    abus_loop();
 }
