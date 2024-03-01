1b920e745c6c05cb6e2d0f5e82b9e4dd2256f4a1
Mark Aikens <17986295+markadev@users.noreply.github.com>
2024-03-01 08:06:12 -0500

    Update videx soft video switch
    Update the Videx 80-column mode switch check so that 80 column mode
    is only displayed when the "color killer is on", i.e. only if the II+
    video mode is text mode. This matches how the real VideoTerm video
    switch works.

--- pico/render.c
+++ pico/render.c
@@ -8,39 +8,41 @@ void render_init() {
 
 
 void render_loop() {
-    while(1) {
 #ifdef RENDER_TEST_PATTERN
+    while(1) {
         render_vga_testpattern();
-#else
-
-#ifdef APPLE_MODEL_IIPLUS
-        if(card_videx_80col) {
-            update_videx_text_flasher();
-            render_videx_text();
-        } else {
-#endif
-            update_text_flasher();
-
-            switch(soft_switches & SOFTSW_MODE_MASK) {
-            case 0:
-                render_lores();
-                break;
-            case SOFTSW_MIX_MODE:
-                render_mixed_lores();
-                break;
-            case SOFTSW_HIRES_MODE:
-                render_hires(false);
-                break;
-            case SOFTSW_HIRES_MODE | SOFTSW_MIX_MODE:
-                render_hires(true);
-                break;
-            default:
-                render_text();
-                break;
-            }
-#ifdef APPLE_MODEL_IIPLUS
-        }
-#endif
-#endif
     }
+#else
+    while(1) {
+        update_text_flasher();
+#ifdef APPLE_MODEL_IIPLUS
+        update_videx_text_flasher();
+#endif
+
+        switch(soft_switches & SOFTSW_MODE_MASK) {
+        case 0:
+            render_lores();
+            break;
+        case SOFTSW_MIX_MODE:
+            render_mixed_lores();
+            break;
+        case SOFTSW_HIRES_MODE:
+            render_hires(false);
+            break;
+        case SOFTSW_HIRES_MODE | SOFTSW_MIX_MODE:
+            render_hires(true);
+            break;
+        default:
+#ifdef APPLE_MODEL_IIPLUS
+            if(card_videx_80col) {
+                render_videx_text();
+            } else
+#endif
+            {
+                render_text();
+            }
+            break;
+        }
+    }
+#endif
 }
